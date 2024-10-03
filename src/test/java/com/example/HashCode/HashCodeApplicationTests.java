e6cb0ed872d8371ad2feab46ca326e324b54d6e7
Yaroslav <programhaking@gmail.com>
2024-10-03 18:25:29 +0300

    Version 0.1, Echo bot

---
+++ src/test/java/com/example/HashCode/HashCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HashCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
